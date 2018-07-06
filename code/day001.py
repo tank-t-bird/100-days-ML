4efe3dc6e046ad3cfa7bb3bd9cd54081007ed299
Tank Thunderbird <tank.thunderbird@gmail.com>
2018-07-06 22:29:47 +0200

    first code file

--- code/day001.py
+++ code/day001.py
@@ -1,3 +1,7 @@
+# Tank Thunderbird 100-days-ML Day 001
+# Since I am looking at vectors, I decided to look at matplotlib and plot a few.
+# W for the Win ;-)
+
 import matplotlib.pyplot as plt
 
 plt.plot([1,2,3,4,5], [3,2,3,2,3])

dbf7845c075cc6bcd779d14c014cbca258f0cd35
Tank Thunderbird <tank.thunderbird@gmail.com>
2018-07-08 13:33:30 +0200

    basic matrix structure

--- code/day003/CLC_Rendering_Matrices/Assets/Scripts/TransformationGrid.cs
+++ code/day003/CLC_Rendering_Matrices/Assets/Scripts/TransformationGrid.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class TransformationGrid : MonoBehaviour
+{
+	public Transform Prefab;
+	public int GridResolution = 10;
+	private Transform[ ] _grid;
 
-	// Use this for initialization
-	void Start () {
-		
+	private void Awake()
+	{
+		_grid = new Transform[GridResolution * GridResolution * GridResolution];
+		for (int i = 0, z = 0; z < GridResolution; z++)
+		{
+			for (int y = 0; y < GridResolution; y++)
+			{
+				for (int x = 0; x < GridResolution; x++, i++)
+				{
+					_grid[i] = CreateGridPoint(x, y, z);
+				}
+					
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private Transform CreateGridPoint(int x, int y, int z)
+	{
+		Transform point = Instantiate(Prefab);
+		point.localPosition = GetCoordinates(x, y, z);
+		point.GetComponent<MeshRenderer>().material.color = new Color(
+			(float)x / GridResolution,
+			(float)y / GridResolution,
+			(float)z / GridResolution
+			);
+		return point;
+
+	}
+
+	//this builds the coordiantes around the center
+	private Vector3 GetCoordinates(int x, int y, int z) 
+	{
+		return new Vector3(
+			x - (GridResolution - 1) * 0.5f,
+			y - (GridResolution - 1) * 0.5f,
+			z - (GridResolution - 1) * 0.5f
+		);
 	}
 }

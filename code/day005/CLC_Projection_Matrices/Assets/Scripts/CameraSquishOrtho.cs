776425cc2a9e73abebbc981c89cdecbf117a9fbe
Tank Thunderbird <tank.thunderbird@gmail.com>
2018-07-10 10:31:31 +0200

    added projection camera scripts

---
+++ code/day005/CLC_Projection_Matrices/Assets/Scripts/CameraSquishOrtho.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraSquishOrtho : Transformation {
+
+    public override Matrix4x4 Matrix
+    {
+        get
+        {
+            Matrix4x4 matrix = new Matrix4x4();
+            matrix.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
+            matrix.SetRow(1, new Vector4(0f, 1f, 0f, 0f));
+            matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
+            matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+            return matrix;
+        }
+    }
+}
---
+++ code/day005/CLC_Projection_Matrices/Assets/Scripts/CameraSquishPersp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraSquishPersp : Transformation {
+	public float FocalLength = 1f;
+	
+	public override Matrix4x4 Matrix
+	{
+		get
+		{
+			Matrix4x4 matrix = new Matrix4x4();
+			matrix.SetRow(0, new Vector4(FocalLength, 0f, 0f, 0f));
+			matrix.SetRow(2, new Vector4(0f, FocalLength, 0f, 0f));
+			matrix.SetRow(3, new Vector4(0f, 0f, 1f, 0f));
+			return matrix;
+		}
+	}
+}
